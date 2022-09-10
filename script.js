b4b2060b7033ef4e0a16dfd555ebe6065f66effb
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-09-09 19:56:14 -0500

    modificacion, uso de addEventListener

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
-const btnCalcular = document.querySelector('btnCalcular');
+const btnCalcular = document.querySelector('#btnCalcular');
 const pResultado = document.querySelector('#resultado');
+
+btnCalcular.addEventListener('click', calcular);
 
 function calcular(){
     let resultado = parseInt(input1.value) + parseInt(input2.value);
